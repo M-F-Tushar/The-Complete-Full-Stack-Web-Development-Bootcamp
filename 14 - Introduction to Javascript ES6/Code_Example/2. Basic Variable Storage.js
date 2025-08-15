e7cfb6d36f435369988c9768b2496f487d63e1d4
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-15 08:14:37 +0600

    Add files via upload

---
+++ 14 - Introduction to Javascript ES6/Code_Example/2. Basic Variable Storage.js
@@ -0,0 +1,7 @@
+var gameLevel = 1;
+alert("Your level is currently " + gameLevel);
+
+gameLevel = 2;
+alert("Your level is now " + gameLevel);
+
+// alert is for browser, use console.log for run in IDE
