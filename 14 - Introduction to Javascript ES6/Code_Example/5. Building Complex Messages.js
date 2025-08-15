d35dbe1d9e9056938e688c8cc2525242ac15694c
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-15 10:45:39 +0600

    Add files via upload

---
+++ 14 - Introduction to Javascript ES6/Code_Example/5. Building Complex Messages.js
@@ -0,0 +1,7 @@
+var firstName = "John";
+var lastName = "Smith";
+var age = 25;
+
+var fullMessage = "Hello, my name is " + firstName + " " + lastName + " and I am " + age + " years old.";
+
+console.log(fullMessage);
