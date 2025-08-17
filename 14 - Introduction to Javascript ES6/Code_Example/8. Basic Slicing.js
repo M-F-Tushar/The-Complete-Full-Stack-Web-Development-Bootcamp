8081e7037d955cdec95508705dafc24158e4f477
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-17 07:48:48 +0600

    Add files via upload

---
+++ 14 - Introduction to Javascript ES6/Code_Example/8. Basic Slicing.js
@@ -0,0 +1,13 @@
+var name = "Angela";
+
+name.slice(0, 1);
+console.log(name.slice(0, 1)); // Outputs: A
+
+name.slice(5, 6);
+console.log(name.slice(5, 6)); // Outputs: a
+
+name.slice(0, 3);
+console.log(name.slice(0, 3)); // Outputs: Ang
+
+name.slice(1, 5);
+console.log(name.slice(1, 5)); // Outputs: ngel
