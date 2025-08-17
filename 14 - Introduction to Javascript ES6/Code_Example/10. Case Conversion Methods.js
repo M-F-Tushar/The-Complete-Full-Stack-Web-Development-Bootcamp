220ba891f07837bc8b33e8aba68c6b7ac459e960
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-17 08:24:59 +0600

    Add files via upload

---
+++ 14 - Introduction to Javascript ES6/Code_Example/10. Case Conversion Methods.js
@@ -0,0 +1,11 @@
+var name = "Angela";
+name.toUpperCase();
+
+name = name.toUpperCase();// Change permanently
+console.log(name);
+
+var name = "ANGELA";
+name.toLowerCase();
+
+name = name.toLowerCase();
+console.log(name);
